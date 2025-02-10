9f31e655b3cee5e5436b756061e87db55227f1c7
Sebastian Wolff <bastiwolff432@gmail.com>
2025-02-10 19:00:15 +0100

    added landingpage + started with customization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './main/game/game.component';
+import { LandingpageComponent } from './main/landingpage/landingpage.component';
 
 export const routes: Routes = [
-    {path: '', component: GameComponent},
+  { path: '', component: LandingpageComponent },
+  { path: 'game', component: GameComponent },
 ];
