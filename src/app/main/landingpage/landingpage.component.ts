034087263820d57194dbb0b5406653f1a811c863
Sebastian Wolff <bastiwolff432@gmail.com>
2025-02-15 14:41:22 +0100

    added landing page chose screen , still struggling with best color combinations

--- src/app/main/landingpage/landingpage.component.ts
+++ src/app/main/landingpage/landingpage.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { GameComponent } from "../game/game.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [GameComponent],
+  imports: [GameComponent, CommonModule],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
 export class LandingpageComponent {
 
   isPlayGamePressed: boolean = false;
+  isChoosing: boolean = true;
+
+  startGame() {
+    this.isChoosing = !this.isChoosing
+    this.isPlayGamePressed = !this.isPlayGamePressed
+  }
+
+  openInstructions() {
+    
+  }
 
 }
