7d30296603ce6bf39d0073bdf0becc6a749322e9
Sebastian Wolff <bastiwolff432@gmail.com>
2025-03-04 19:04:26 +0100

    added some repsonsive

--- src/app/main/landingpage/landingpage.component.ts
+++ src/app/main/landingpage/landingpage.component.ts
@@ -1,35 +1,62 @@
 import { Component, HostListener } from '@angular/core';
-import { GameComponent } from "../game/game.component";
+import { GameComponent } from '../game/game.component';
 import { CommonModule } from '@angular/common';
-import { InstructionsComponent } from "./instructions/instructions.component";
-import { ManualComponent } from "./manual/manual.component";
+import { InstructionsComponent } from './instructions/instructions.component';
+import { ManualComponent } from './manual/manual.component';
 import { RouterLink } from '@angular/router';
 import { GlobalstateserviceService } from '../../services/globalstate.service';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-landingpage',
-  imports: [GameComponent, CommonModule, InstructionsComponent, ManualComponent, RouterLink],
+  imports: [
+    GameComponent,
+    CommonModule,
+    InstructionsComponent,
+    ManualComponent,
+    RouterLink,
+  ],
   templateUrl: './landingpage.component.html',
-  styleUrl: './landingpage.component.scss'
+  styleUrl: './landingpage.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1 })),
+      state('out', style({ opacity: 0 })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('500ms ease-out', style({ opacity: 0})),
+      ]),
+    ]),
+  ],
 })
 export class LandingpageComponent {
-
   isPlayGamePressed: boolean = false;
   isInstructionsPressed: boolean = false;
-  isManual: boolean = false
+  isManual: boolean = false;
   isChoosing: boolean = true;
   currentHeight!: number;
   isInvis: boolean = false;
+  minWidth: number = 768;
+  isMobilePortrait: boolean = false;
 
-  constructor(public globalStateService: GlobalstateserviceService) { 
+  constructor(public globalStateService: GlobalstateserviceService) {
     this.currentHeight = window.innerHeight;
     this.checkInvis(window.innerHeight);
+    this.checkScreenSize(window.innerWidth, window.innerHeight);
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.currentHeight = event.target.innerHeight;
     this.checkInvis(event.target.innerHeight);
+    this.checkScreenSize(event.target.innerWidth, event.target.innerHeight);
+  }
+
+  checkScreenSize(width: number, height: number) {
+    const isPortrait = width < height;
+    this.isMobilePortrait = width < this.minWidth && isPortrait;
   }
 
   checkInvis(height: number) {
@@ -41,22 +68,21 @@ export class LandingpageComponent {
   }
 
   startGame() {
-    this.isChoosing = !this.isChoosing
-    this.isPlayGamePressed = !this.isPlayGamePressed
+    this.isChoosing = !this.isChoosing;
+    this.isPlayGamePressed = !this.isPlayGamePressed;
   }
 
   openInstructions() {
-    this.isChoosing = !this.isChoosing
+    this.isChoosing = !this.isChoosing;
     this.isManual = false;
   }
 
   handleClosing(event: any) {
-    this.isChoosing = !this.isChoosing
+    this.isChoosing = !this.isChoosing;
   }
 
   openManual() {
-    this.isChoosing = !this.isChoosing
-    this.isManual = !this.isChoosing
+    this.isChoosing = !this.isChoosing;
+    this.isManual = !this.isChoosing;
   }
-
 }
