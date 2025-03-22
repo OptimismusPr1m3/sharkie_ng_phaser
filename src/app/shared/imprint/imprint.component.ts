3053327a422e901a5579994f7704ff14ac3dfb63
Sebastian Wolff <bastiwolff432@gmail.com>
2025-03-22 20:55:06 +0100

    added imprint section

--- src/app/shared/imprint/imprint.component.ts
+++ src/app/shared/imprint/imprint.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [CommonModule, MatIconModule, RouterModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
